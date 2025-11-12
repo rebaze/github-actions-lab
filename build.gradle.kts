a053bca5c7cad2abdabdcf02a0d72295e652f44a
Toni Menzel <toni.menzel@rebaze.com>
2025-11-12 22:00:59 +0100

    Update Spring Boot and dependency management versions; upgrade Java toolchain and Gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.3"
-	id("io.spring.dependency-management") version "1.1.6"
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+    id("dev.sigstore.sign") version "2.0.0-rc2"
 }
 
 group = "com.example"
@@ -9,7 +10,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(25)
 	}
 }
 
